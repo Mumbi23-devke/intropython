0029e747c4405aba3d87a904d3e53d9a264ef3a8
eMobilis <gloriawaweru5@gmail.com>
2023-06-23 10:04:20 +0300

    changed contact name in website

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("Hello again world")
+print("My name is Mumbi")
 
---
+++ variables.py
@@ -0,0 +1,11 @@
+city = "Nakuru"
+print(city)
+age = "38"
+print(age)
+name = "Mumbi"
+print(name)
+x = "28"
+y = "18"
+print(y)
+print(x+y)
+print(x)
